5f337e7510a8e0599e9b02858bcd0f95060ac943
GitDohyun <gitdohyun@gmail.com>
2024-06-17 10:28:57 +0900

    프로젝트 생성

---
+++ Norou/Norou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Norou
+//
+//  Created by 김도현 on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Norou/Norou/NorouApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorouApp.swift
+//  Norou
+//
+//  Created by 김도현 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NorouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
